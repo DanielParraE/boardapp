9e8aa75e6bd5e75c676722b8526e607aaaa81df5
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2025-03-03 13:43:18 -0700

    first commit

---
+++ boardapp/__init__.py
@@ -0,0 +1,5 @@
+"""
+test board app
+"""
+
+__version__ = '0.1.0'
---
+++ boardapp/apps.py
@@ -0,0 +1,26 @@
+"""
+boardapp Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class BoardappConfig(AppConfig):
+    """
+    Configuration for the boardapp Django application.
+    """
+
+    name = "boardapp"
+    plugin_app = {
+        "url_config": {
+            "lms.djangoapp": {
+                "namespace": "boardapp",
+                "relative_path": "urls",
+            }
+        },
+        "settings_config": {
+            "lms.djangoapp": {
+                "common": {"relative_path": "settings"},
+            }
+        },
+    }
