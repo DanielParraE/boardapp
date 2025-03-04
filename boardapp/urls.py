4f037bbedaee3edc308551ad421c4674da11fcce
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2025-03-04 11:05:57 -0700

    chore: changed url

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -6,5 +6,5 @@ from .views import BoardappIndex
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
-    path("index/", BoardappIndex.as_view(), name="index"),
+    path("ba-index/", BoardappIndex.as_view(), name="index"),
 ]
