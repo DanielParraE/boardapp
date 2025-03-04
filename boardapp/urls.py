e972c72f9b9e1eedc5a5584aaebce022f0fe1ed2
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2025-03-04 11:01:38 -0700

    chore: changed url

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,10 +1,10 @@
 """
 URLs for boardapp.
 """
-from django.urls import re_path  # pylint: disable=unused-import
+from django.urls import path  # pylint: disable=unused-import
 from .views import BoardappIndex
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
-    re_path(r"", BoardappIndex.as_view(), name="index"),
+    path("index/", BoardappIndex.as_view(), name="index"),
 ]
